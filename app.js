f3930ced4ac2b5474d0fe2fb486fbea77166239b
Nikhil Gavali <gavalinikhil700@gmail.com>
2020-07-04 18:21:05 +0530

    resolved db issue

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const deviceRouter = require('./routes/deviceis-routes');
 
+const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 var url = process.env.MONGO_URL;
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('mongo connection successful'))
-    .catch((err) => console.error(err));
 
-const app = express();
+function db() {
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('mongo connection successful'))
+        .catch((err) => console.error(err));
+}
+
+db();
 
 app.use('*', cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/device', deviceRouter);
 
